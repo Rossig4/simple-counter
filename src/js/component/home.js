19ddc7db2135b8cdad5b7ee687db9f5693522805
Rossig4 <r.silvera.ag@gmail.com>
2021-05-05 18:11:58 +0000

    origin

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,32 +1,42 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //include images into your bundle
 
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
 		<div className="flex-container">
 			<div className="watch">
 				<i className="far fa-clock"></i>
 			</div>
 			<div className="watch">
-				<h1 className="numero">0</h1>
+				<h1 className="numero">{props.numSeis}</h1>
 			</div>
 			<div className="watch">
-				<h1>0</h1>
+				<h1>{props.numCinco}</h1>
 			</div>
 			<div className="watch">
-				<h1>0</h1>
+				<h1>{props.numCuatro}</h1>
 			</div>
 			<div className="watch">
-				<h1>0</h1>
+				<h1>{props.numTres}</h1>
 			</div>
 			<div className="watch">
-				<h1>0</h1>
+				<h1>{props.numDos}</h1>
 			</div>
 			<div className="watch">
-				<h1>0</h1>
+				<h1>{props.numUno}</h1>
 			</div>
 		</div>
 	);
 }
+
+Home.propTypes = {
+	numUno: PropTypes.string,
+	numDos: PropTypes.string,
+	numTres: PropTypes.string,
+	numCuatro: PropTypes.string,
+	numCinco: PropTypes.string,
+	numSeis: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,25 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let contador = 0;
+setInterval(() => {
+	let numUno = Math.floor(contador / 1) % 10;
+	let numDos = Math.floor(contador / 10) % 10;
+	let numTres = Math.floor(contador / 100) % 10;
+	let numCuatro = Math.floor(contador / 1000) % 10;
+	let numCinco = Math.floor(contador / 10000) % 10;
+	let numSeis = Math.floor(contador / 100000) % 10;
+	contador++;
+	ReactDOM.render(
+		<Home
+			numUno={numUno}
+			numDos={numDos}
+			numTres={numTres}
+			numCuatro={numCuatro}
+			numCinco={numCinco}
+			numSeis={numSeis}
+		/>,
+		document.querySelector("#app")
+	);
+}, 10);
