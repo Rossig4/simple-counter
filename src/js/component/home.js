2f54dbce7cb270093dadf4c47ff2799a827d6817
Rossig4 <r.silvera.ag@gmail.com>
2021-05-05 17:30:45 +0000

    subiendo ejercicio

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,7 +7,10 @@ export function Home() {
 	return (
 		<div className="flex-container">
 			<div className="watch">
-				<h1>0</h1>
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="watch">
+				<h1 className="numero">0</h1>
 			</div>
 			<div className="watch">
 				<h1>0</h1>
